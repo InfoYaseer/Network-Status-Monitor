eb0af7f5fcd32ec426920234a030688e7ef1f450
Mohamed Yaseer <160985940+InfoYaseer@users.noreply.github.com>
2026-02-14 20:46:43 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+window.addEventListener('online', updateStatus);
+window.addEventListener('offline', updateStatus);
+
+function updateStatus(){
+
+    const status = document.getElementById('status');
+    const userStatus = document.querySelector('.user-status');
+    const content = document.querySelector('.content');
+    const msg = document.querySelector('.msg');
+
+    if(navigator.onLine){
+
+        status.textContent = "Online";
+        userStatus.classList.remove('offline');
+        userStatus.classList.add('online');
+
+        content.style.display = "flex";
+        msg.style.display = "none";
+
+    } else {
+
+        status.textContent = "Offline";
+        userStatus.classList.remove('online');
+        userStatus.classList.add('offline');
+
+        content.style.display = "none";
+        msg.style.display = "block";
+    }
+}
+
+updateStatus();
